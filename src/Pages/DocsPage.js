03f4090191a78bee1ce0ba70f09431e9cbc62a40
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2021-07-23 23:47:15 -0300

    Beta

--- src/Pages/DocsPage.js
+++ src/Pages/DocsPage.js
@@ -14,42 +14,42 @@ import home from '../assets/icons/home.svg'
 import componentes from "../assets/icons/stacked-files.svg"
 
 const DocsPage = () => {
-    return (
-        <>
-            
-    <div className="full-container">
-      <NavbarAside> 
-        <span className='m-2'>FaqStyle</span>
-        <a className='m-2' href="#home"><img src={home} alt="Inicio" />Inicio</a>
-        <a href="#works"><img src={componentes} alt="works" />Formularios</a>
-        <a href="#works"><img src={componentes} alt="works" />Componentes</a>
-        <ul className='mt-0'>
-          <li className='m-2'><a href="#works">Alerts </a></li>
-          <li className='m-2'><a href="#works">Buttons</a></li>
-          <li className='m-2'><a href="#works">Buttons Group</a></li>
-          <li className='m-2'><a href="#works">Card</a></li>
-          <li className='m-2'><a href="#works">Carousel</a></li>
-          <li className='m-2'><a href="#works">Dropdowns</a></li>
-          <li className='m-2'><a href="#works">List Group</a></li>
-          <li className='m-2'><a href="#works">Modal</a></li>
-          <li className='m-2'><a href="#works">Navbar</a></li>
-          <li className='m-2'><a href="#works">Progress</a></li>
-        </ul>
-      </NavbarAside>
-      <div className="main-container">
 
+  return (
+    <>
+      <div className="full-container">
+
+        <NavbarAside> 
+          <span className='m-2'>FaqStyle</span>
+          <a className='m-2' href="#home"><img src={home} alt="Inicio" />Inicio</a>
+          <a href="#works"><img src={componentes} alt="works" />Formularios</a>
+          <a href="#works"><img src={componentes} alt="works" />Componentes</a>
+          <ul className='mt-0'>
+            <li className='m-2'><a href="#works">Alerts </a></li>
+            <li className='m-2'><a href="#works">Buttons</a></li>
+            <li className='m-2'><a href="#works">Buttons Group</a></li>
+            <li className='m-2'><a href="#works">Card</a></li>
+            <li className='m-2'><a href="#works">Carousel</a></li>
+            <li className='m-2'><a href="#works">Dropdowns</a></li>
+            <li className='m-2'><a href="#works">List Group</a></li>
+            <li className='m-2'><a href="#works">Modal</a></li>
+            <li className='m-2'><a href="#works">Navbar</a></li>
+            <li className='m-2'><a href="#works">Progress</a></li>
+          </ul>
+        </NavbarAside>
+        <div className="main-container">
         <FormPage />
-        <AlertPage />
-        <ButtonPage />
-        <CardPage />
-        <ModalPage />
-        <NavbarPage />
-        <ProgresBarPage />
-        <FaqFooter />
+          <AlertPage />
+          <ButtonPage />
+          <CardPage />
+          <ModalPage />
+          <NavbarPage />
+          <ProgresBarPage />
+          <FaqFooter />
+        </div>
       </div>
-    </div>
-        </>
-    )
+    </>
+  )
 }
 
 export default DocsPage
