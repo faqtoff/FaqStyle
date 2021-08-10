f0a18655102156175600a8a40d436deae49b4e6d
Facundo Toffolo Pasquini <f.toff.pas@gmail.com>
2021-08-10 03:41:01 -0300

    beta

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ function App() {
         <h1 className='fs-banner-info__title'>FaqStyle <span className='color-gray-500'>by <a href="https://www.faqtoff.com.ar" target='_blank'>@FaqToff</a></span></h1>
         <a className="btn btn-rounded" href="#main">Empezar</a>
       </BannerFullScreen>
-      <div className="container-fluid" id='main'>
-        <h2>Hola Mundo</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, sint! Eum, officiis, quae doloribus expedita qui beatae eaque explicabo earum, reiciendis ipsum libero perspiciatis quo nesciunt facere quas illo labore?</p>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Praesentium laudantium earum quas eius, officiis ipsum nostrum natus cum repudiandae vitae dolore facere nulla animi enim veritatis dolores! Cum, error molestias.</p>
-      </div>
       <DocsPage />
       </div>
     </div>
--- src/Pages/AlertPage.js
+++ src/Pages/AlertPage.js
@@ -6,44 +6,140 @@ const AlertPage = () => {
         <div className='container-sm mt-5'>
             <h2>Alertas</h2>
 
-            <div className='alert'>
-                <h2>Alerta</h2>
-            </div>
+            <div className='d-flex'>
+                <div className='w-stretch'>
+                    <div className='alert m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-primary'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-primary m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-secondary'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-secondary m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-success'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-success m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-info'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-info m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-warning'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-warning m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-danger'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-danger m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-light'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-light m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-dark'>
-                <h2>Alerta</h2>
-            </div>
+                    <div className='alert alert-dark m-1'>
+                        <h2>Alerta</h2>
+                    </div>
 
-            <div className='alert alert-especial'>
-                <h2>Alerta</h2>
+                    <div className='alert alert-especial m-1'>
+                        <h2>Alerta</h2>
+                    </div>
+                </div>
+
+                <div>
+                    <pre className='alert alert-secondary m-1 p-2 d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                    <pre className='d-flex flex-col'>
+                        <code>
+                            &lt;div class='alert m-1'&gt;
+                        </code>
+                        <code>
+                            &lt;h2&gt;Alerta&lt;/h2&gt;
+                        </code>
+                        <code>
+                            &lt;/div&gt;
+                        </code>
+                    </pre>
+                </div>
             </div>
         </div>
     )
